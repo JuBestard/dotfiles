e0f275baae6b5d6b2fa6d742ed6dda604cf6aacd
Julien Bestard <julien.bestard@epita.fr>
2022-02-19 12:27:59 +0100

    i3 lock

---
+++ i3-lock.sh
@@ -0,0 +1,26 @@
+rectangles=" "
+
+SR=$(xrandr --query | grep ' connected' | grep -o '[0-9][0-9]*x[0-9][0-9]*[^ ]*')
+for RES in $SR; do
+  SRA=(${RES//[x+]/ })
+  CX=$((${SRA[2]} + 25))
+  CY=$((${SRA[1]} - 80))
+  rectangles+="rectangle $CX,$CY $((CX+300)),$((CY-80)) "
+done
+
+TMPBG=/tmp/screen.png
+scrot $TMPBG && convert $TMPBG -scale 5% -scale 2000% -draw "fill black fill-opacity 0.4 $rectangles" $TMPBG
+
+i3lock \
+  -i $TMPBG \
+  --force-clock --indicator \
+  --time-pos="x+120:h-120" \
+  --date-str "Type password to unlock..." \
+  --inside-color=00000000 --ring-color=ffffffff --line-uses-inside \
+  --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
+  --insidever-color=fecf4dff --insidewrong-color=d23c3dff \
+  --ringver-color=ffffffff --ringwrong-color=ffffffff --ind-pos="x+290:h-120" \
+  --radius=20 --ring-width=3 --verif-text=" " --wrong-text=" " \
+  --time-color="ffffffff" --date-color="ffffffff"
+
+rm $TMPBG
